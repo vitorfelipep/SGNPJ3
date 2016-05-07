12fb20cc097c3e515e99c13cf2ab7d93c2c693e8
vitorfelipep <vitorfelipep@gmail.com>
2016-05-07 11:45:38 -0300

    Correções de bug e desenvolvimento da edição do atendimento.

---
+++ src/main/java/com/sgnpj/converter/AtendimentoConverter.java
@@ -0,0 +1,45 @@
+package com.sgnpj.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import com.sgnpj.model.Atendimento;
+import com.sgnpj.repository.Atendimentos;
+
+public class AtendimentoConverter implements Converter{
+	
+	private Atendimentos atendimentos;
+	
+	public AtendimentoConverter() {
+		this.atendimentos = com.sgnpj.util.cdi.CDIServiceLocator.getBean(Atendimentos.class);
+	}
+	
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		
+		Atendimento retorno = null;
+		
+		if(value != null){
+			Long id = new Long(value);
+			retorno = atendimentos.porId(id);
+		}
+		
+		return retorno;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		
+		if(value != null){
+			Atendimento atendimento = (Atendimento) value;
+			
+			return atendimento.getId() == null ? null : atendimento.getId().toString();
+		}
+		
+		return "";
+	}
+
+}
